2116103a8bd80e2265b33d1fc20931912c051fa1
tmliao <47618238+tmliao@users.noreply.github.com>
2019-02-14 11:37:11 +0800

    Update pushmsg.php

--- pushmsg.php
+++ pushmsg.php
@@ -20,7 +20,7 @@
   
     fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
     $header[] = "Content-Type: application/json";
-    $header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+    $header[] = "Authorization: Bearer RinZuAxd/Mro8TiOyUHjyL6SVy0NhwLQk6h+NKz4WojC2gIIx0Nzcp0waryjPR58967RkawdWLtjSOyGKbF/SGk0A/ZJGxo6VSkeHqzWByPZF0Uo8w6Cw7u0R7AG4tvD5MAxgcbqVf3SHk9YxRvHwwdB04t89/1O/w1cDnyilFU=";
     $ch = curl_init("https://api.line.me/v2/bot/message/push");
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
